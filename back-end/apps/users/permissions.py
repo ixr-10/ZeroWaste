fb51170d4a948bb4d497b18d58c3032314b70529
ichrak <ni.dallaa@esi-sba.dz>
2026-02-27 23:35:29 +0100

    .

--- back-end/apps/users/permissions.py
+++ back-end/apps/users/permissions.py
@@ -4,7 +4,7 @@ class IsAdmin(BasePermission):
     def has_permission(self, request, view):
         return request.user.is_authenticated and request.user.role == 'admin'
 
-class Isdonateur(BasePermission):
+class IsDonateur(BasePermission):
     def has_permission(self, request, view):
         return request.user.is_authenticated and request.user.role == 'donateur'
 
