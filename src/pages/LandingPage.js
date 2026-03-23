66a1f009ff0ac7c47b0f0dd4a9d9fc6c768284d6
saja-sef <s.sefiane@esi-sba.dz>
2026-03-23 05:14:02 +0100

    Final version of projet1cs

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../styles/LandingPage.css'; 
+import logo from '../assets/Logo.png'; 
+
+function LandingPage({ onNavigate }) {
+  return (
+    
+    <div className="landing-page" onClick={onNavigate} style={{ cursor: 'pointer' }}>
+      <div className="logo-group">
+        <img src={logo} alt="Logo" className="main-logo" />
+        <h2 className="zero-waste-text">ZER0 WASTE</h2>
+      </div>
+      <h1 className="admin-dashboard-text">ADMIN DASHBOARD</h1>
+    </div>
+  );
+}
+
+export default LandingPage;
