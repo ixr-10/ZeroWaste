cae7c10d9bfc8a310a05a263a2721301ab4cf184
bendali-ibtihal-layal <il.bendali@esi-sba.dz>
2026-03-23 01:57:18 +0100

    fix: keyboard hides input fields in login screen AND writing hides icon

---
+++ front-end/components/Header.tsx
@@ -0,0 +1,40 @@
+
+import { Image,  TouchableOpacity, View, StyleSheet} from "react-native";
+import { Ionicons } from '@expo/vector-icons';
+import { router } from 'expo-router';
+
+
+export default function header ({showBack = false}){
+  return (
+    <View style={styles.container}>
+      {showBack ? (
+          <TouchableOpacity
+            onPress={() => router.back()}   // goes back to previous screen
+            
+            >
+            <Ionicons name="arrow-back-circle-outline" size={32} color="black" style={{top: 20,}} />
+          </TouchableOpacity>
+      ) : (
+         <View style={{ width: 32 }} />
+      )}
+      <Image source={require('../assets/images/logo.png')} style={styles.logo}/>
+    </View>
+  )
+
+}
+
+const styles = StyleSheet.create({
+  logo : {width:36,height:29.03,resizeMode: 'contain',  // ⬅️ key
+    top: 20,               // distance from top
+    right: 20,},
+   container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    marginBottom: 25,
+    paddingTop: 0,
+    marginTop: 0,
+  },
+
+});
